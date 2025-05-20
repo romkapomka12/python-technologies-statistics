cc59d801c4d7e8f170b34748961c4a122bf42b54
Roman Burluka <103953904+romkapomka12@users.noreply.github.com>
2025-05-20 17:27:59 +0300

    bypassing parsing blocking by changing the user agent in a random way

--- config/config.py
+++ config/config.py
@@ -1,12 +1,26 @@
+import random
 
 TECHNOLOGIES = [
     "Python", "Django", "Flask", "PostgreSQL", "Docker", "AWS",
     "JavaScript", "React", "FastAPI", "Git", "Linux", "Celery"
 ]
 
-JOB_SEARCH_URL = "https://jobs.dou.ua/vacancies/?search=python"
-HEADERS = {
-    "User-Agent": "Mozilla/5.0"
-}
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 "
+    "Safari/605.1.15",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/115.0",
+]
+
+
+def get_random_headers():
+    return {
+        "User-Agent": random.choice(USER_AGENTS)
+    }
+
+
+JOB_SEARCH_URL = "https://jobs.dou.ua/vacancies/?category=Python"
 
 CSV_OUTPUT_PATH = "data/processed/output.csv"
